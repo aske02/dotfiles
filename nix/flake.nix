49e73ca77df3759ef49b9e3f3a09eed199805e6b
Felix Berger <felix.enok.berger@gmail.com>
2025-02-01 00:20:33 +0100

    Refactor Nix configuration for improved structure and added WSL system support

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,130 +3,60 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
-
-      services.nix-daemon.enable = true;
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [
-        pkgs.zed-editor
-        pkgs.nixd
-        pkgs.nil
-        pkgs.git
-        pkgs.gh
-        pkgs.starship
-        pkgs.raycast
-        pkgs.tmux
-        pkgs.fzf
-        pkgs.spotify
-      ];
-
-      # Fonts
-      fonts.packages = [
-        pkgs.nerd-fonts.jetbrains-mono
-      ];
-
-      #homebrew
-      homebrew = {
-        enable = true;
-        casks = [
-          "1password"
-          "discord"
-          "arc"
-          "docker"
-          "notion"
-        ];
-        onActivation = {
-          autoUpdate = true;
-          cleanup = "uninstall";
-          upgrade = true;
-        };
-        brews = [
-          "pyenv"
-        ];
-      };
-
-      #Users
-      users.users = {
-        "felix.berger" = {
-          name = "felix.berger";
-          home = "/Users/felix.berger";
-        };
-      };
-      nix.configureBuildUsers = true;
-      nix.useDaemon = true;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.tilesize = 48;
-        dock.magnification = false;
-        dock.persistent-apps = [
-          "/System/Applications/Launchpad.app"
-          "/Applications/Arc.app"
-          "/Applications/Discord.app"
-          "/Applications/Ghostty.app"
-          "${pkgs.zed-editor}/Applications/Zed.app"
-          "/System/Applications/System Settings.app"
-        ];
-
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-
-        trackpad.TrackpadRightClick = true;
-
-        controlcenter.BatteryShowPercentage = true;
-
-        NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = true;
-      };
-
-
-      # Enable sudo touch id authentication
-      security.pam.enableSudoTouchIdAuth = true;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
+  outputs = inputs@{ self, nix-darwin. nixos-wsl, nixpkgs, home-manager }:
   {
+    homeConfigurations = {
+      "Felixs-MacBook-Air" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [
+          ./hosts/home.nix
+          ./hosts/darwin/home.nix
+        ];
+      };
+
+      wsl = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./hosts/home.nix
+          #./hosts/wsl/home.nix
+        ];
+      };
+    };
+
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#personal
     darwinConfigurations."Felixs-MacBook-Air" = nix-darwin.lib.darwinSystem {
       modules = [
-        configuration
-        home-manager.darwinModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users."felix.berger" = import ./home.nix;
-          };
-        }
+        ./hosts/system.nix
+        ./hosts/darwin/darwin.nix
       ];
+    };
+
+    # Build nixosConfigurations using:
+    #
+    nixosConfigurations = {
+      wsl = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/system.nix
+          ./hosts/wsl/wsl.nix
+        ];
+      };
     };
 
     darwinPackages = self.darwinConfigurations."personal".pkgs;
---
+++ nix/hosts/wsl/wsl.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }: {
+    wsl.enable = true;
+    wsl.defaultUser = "nixos";
+    wsl.wslConf.network.generateResolvConf = false;
+
+    # Enable Docker
+    virtualisation.docker.enable = true;
+    users.users.nixos.extraGroups = [ "docker" ];
+
+    services.resolved = {
+        enable = true;
+        dnssec = "false";
+        domains = [ "~." ];
+        dnsovertls = "false";
+    };
+}
