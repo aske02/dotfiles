c3a8f6c9630f5c1d2e430bd860b9b3001323f20d
Felix Berger <felix.enok.berger@gmail.com>
2025-02-01 00:27:52 +0100

    Add home-manager to system packages in system.nix

--- nix/hosts/system.nix
+++ nix/hosts/system.nix
@@ -12,6 +12,7 @@
     # List packages installed in system profile. To search by name, run:
     # $ nix-env -qaP | grep wget
     environment.systemPackages = [
+        pkgs.home-manager
         pkgs.nixd
         pkgs.nil
         pkgs.git
