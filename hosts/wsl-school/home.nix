7e4348af3d183332d59a4639678bd5991aeaa889
Aske <aske020304@gmail.com>
2025-03-06 11:55:42 +0000

    New piperelay for ssh to 1password

--- hosts/wsl-school/home.nix
+++ hosts/wsl-school/home.nix
@@ -8,10 +8,6 @@
     ../../modules/git.nix
   ];
 
-  home.file = {
-    ".ssh_pipe".source = ../../modules/zsh/.ssh_pipe;
-  };
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "nixos";
@@ -19,13 +15,4 @@
 
   programs.git.extraConfig.core.sshCommand = "ssh.exe";
   programs.git.extraConfig.gpg.ssh.program = "/mnt/c/Users/66594/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
-
-  programs.zsh.initExtra = ''
-    source ~/.ssh_pipe
-  '';
-
-  programs.zsh.shellGlobalAliases = {
-    # Temp fix
-    ssh = "ssh.exe";
-  };
 }
