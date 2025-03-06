5e5f90cd9af05d4126a29d1270ce977ede51a9fe
Aske <aske020304@gmail.com>
2025-03-06 18:56:21 +0000

    Support for home wsl

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -8,19 +8,8 @@
     ../../modules/git.nix
   ];
 
-  home.file = {
-    ".ssh_pipe".source = ../../modules/zsh/.ssh_pipe;
-  };
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
-
-  programs.git.extraConfig.core.sshCommand = "ssh.exe";
-  programs.git.extraConfig.gpg.ssh.program = "/mnt/c/Users/aske0/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
-
-  programs.zsh.initExtra = ''
-    source ~/.ssh_pipe
-  '';
 }
