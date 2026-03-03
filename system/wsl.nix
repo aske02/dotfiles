3ed48d8a396e2ced92487ca1e3753ce1bdd3a9e6
aske <aske020304@gmail.com>
2026-03-04 00:17:05 +0100

    added a fix for zed using 'wsl.exe --exec' which doesnt load enviroments correctly

--- system/wsl.nix
+++ system/wsl.nix
@@ -7,6 +7,12 @@
   wsl.defaultUser = config.var.username;
   wsl.wslConf.network.generateResolvConf = false;
 
+  # Remote server utils in WSL doesnt open interactive shells, so we need to force a link rule for cp
+  # Zed extension remote uses cp to copy recompiled extensions over this way
+  systemd.tmpfiles.rules = [
+    "L+ /bin/cp - - - - /run/current-system/sw/bin/cp"
+  ];
+
   services.resolved = {
     enable = true;
     settings.Resolve = {
