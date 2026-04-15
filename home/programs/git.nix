0e4a78c24900e1f6d6207045f488083e477f704e
aske <aske020304@gmail.com>
2026-04-15 20:37:07 +0200

    hardcoding binary path was wrong

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -23,7 +23,6 @@ in {
         github.user = gh_username;
         push.autoSetupRemote = true;
         gpg.format = "ssh";
-        gpg.ssh.program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
         commit.gpgSign = true;
       };
 
